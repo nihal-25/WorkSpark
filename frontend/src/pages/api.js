3f81e5cf532c230548bade08e6c069970cec6e77
nihal-25 <nihalmanjunath.cs22@bmsce.ac.in>
2025-11-09 01:56:23 +0530

    Fix case-sensitive import for Vercel

--- frontend/src/pages/api.js
+++ frontend/src/pages/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // backend base URL
+  baseURL: "https://workspark.onrender.com", // backend base URL
 });
 
 
